dcfba5ec890fab4614e9aeb57f954fc5e537b4cb
Nikolay Makhonin <nikolay.makhonin@gmail.com>
2021-05-28 08:23:38 +0300

    Develop

--- src/main/common/contracts/new.ts
+++ src/main/common/contracts/new.ts
@@ -1,13 +1,20 @@
 export type TPromiseOrValue<TValue> = Promise<TValue> | TValue
 
+// region common
+
 export enum TRequestMethod {
     Get = 'get',
     GetCount = 'getCount',
     GetRange = 'getRange',
+    Add = 'add',
     Put = 'put',
     Delete = 'delete',
     DeleteRange = 'deleteRange',
 }
+
+// endregion
+
+// region get
 
 export type TGetRequest<TIndex> = {
     type: TRequestMethod.GetCount
@@ -22,8 +29,28 @@ export type TGetRequest<TIndex> = {
     desc?: boolean
 }
 
-export type TChangeRequest<TIndex, TItem> = {
-    type: TRequestMethod.Put
+export type TGetResult<TItem> = {
+    type: TRequestMethod.GetCount
+    count: number
+} | {
+    type: TRequestMethod.Get | TRequestMethod.GetRange
+    items: TItem[]
+}
+
+export type TGetResults<TIndex, TItem> = {
+	requests: TGetRequest<TIndex>
+	results: TGetResult<TItem>
+}
+
+export type TGet<TIndex, TItem> = (requests: TGetRequest<TIndex>[])
+    => TPromiseOrValue<TGetResults<TIndex, TItem>>
+
+// endregion
+
+// region addDelete
+
+export type TAddDeleteRequest<TIndex, TItem> = {
+    type: TRequestMethod.Add
     items: TItem[]
 } | {
     type: TRequestMethod.Delete
@@ -34,12 +61,35 @@ export type TChangeRequest<TIndex, TItem> = {
     indexTo?: TIndex
 }
 
-export type TGetResult<TItem> = {
-    type: TRequestMethod.GetCount
-    count: number
+export type TAddDeleteResult<TIndex> = {
+    type: TRequestMethod.Add
+    indexes: TIndex[]
 } | {
-    type: TRequestMethod.Get | TRequestMethod.GetRange
+    type: TRequestMethod.Delete | TRequestMethod.DeleteRange
+}
+
+export type TAddDeleteResults<TIndex, TItem> = {
+	requests: TAddDeleteRequest<TIndex, TItem>
+	results: TAddDeleteResult<TIndex>
+}
+
+export type TAddDelete<TIndex, TItem> = (requests: TAddDeleteRequest<TIndex, TItem>[])
+    => TPromiseOrValue<TAddDeleteResults<TIndex, TItem>>
+
+// endregion
+
+// region change
+
+export type TChangeRequest<TIndex, TItem> = {
+    type: TRequestMethod.Put | TRequestMethod.Add
     items: TItem[]
+} | {
+    type: TRequestMethod.Delete
+    indexes: TIndex[]
+} | {
+    type: TRequestMethod.DeleteRange
+    indexFrom?: TIndex
+    indexTo?: TIndex
 }
 
 export type TChangeResult<TIndex> = {
@@ -49,24 +99,70 @@ export type TChangeResult<TIndex> = {
     type: TRequestMethod.Delete | TRequestMethod.DeleteRange
 }
 
-export type TGetResults<TIndex, TItem> = {
-	requests: TGetRequest<TIndex>
-	results: TGetResult<TItem>
-}
-
 export type TChangeResults<TIndex, TItem> = {
 	requests: TChangeRequest<TIndex, TItem>
 	results: TChangeResult<TIndex>
 }
 
-export interface IAsyncHeap<TIndex, TItem> {
-    get(requests: TGetRequest<TIndex>[]): TPromiseOrValue<TGetResults<TIndex, TItem>>
-    change(requests: TChangeRequest<TIndex, TItem>[]): TPromiseOrValue<TChangeResults<TIndex, TItem>>
+export type TChange<TIndex, TItem> = (requests: TChangeRequest<TIndex, TItem>[])
+    => TPromiseOrValue<TChangeResults<TIndex, TItem>>
+
+// endregion
+
+// region INonUpdatableAsyncHeap
+
+export interface INonUpdatableAsyncHeap<TIndex, TItem> {
+    readonly get: TGet<TIndex, TItem>
+    readonly addDelete: TAddDelete<TIndex, TItem>
 }
 
+// endregion
+
+// region IAsyncHeap
+
+export interface IAsyncHeap<TIndex, TItem> {
+    readonly get: TGet<TIndex, TItem>
+    /** add === put */
+    readonly change: TChange<TIndex, TItem>
+}
+
+// endregion
+
+// region IChangesAsyncHeap
+
+export type IChangeItem<TIndex, TItem> = {
+    type: TRequestMethod.Put
+    index: TIndex
+    item: TItem
+} | {
+    type: TRequestMethod.Delete
+    index: TIndex
+}
+
+export interface IChangesAsyncHeap<TChangeIndex, TIndex, TItem> extends
+    INonUpdatableAsyncHeap<TChangeIndex, IChangeItem<TIndex, TItem>>
+{ }
+
+export type TGetChanges<TChangeIndex, TIndex, TItem> = TGet<TChangeIndex, IChangeItem<TIndex, TItem>>
+
+// endregion
+
+// region ITransactionAsyncHeap
+
 export interface ITransactionAsyncHeap<TIndex, TItem> {
-    get(requests: TGetRequest<TIndex>[]): TPromiseOrValue<TGetResults<TIndex, TItem>>
-    changeInTransaction(requests: TChangeRequest<TIndex, TItem>[]): TPromiseOrValue<TChangeResults<TIndex, TItem>>
+    readonly getInTransaction: TGet<TIndex, TItem>
+    readonly changeInTransaction: TChange<TIndex, TItem>
     /** Forbidden to use get requests after change requests */
     useTransaction<T>(func: (transaction: IAsyncHeap<TIndex, TItem>) => TPromiseOrValue<T>): TPromiseOrValue<T>
 }
+
+// endregion
+
+// region IDb
+
+export interface IDb<TChangeIndex, TIndex, TItem> extends ITransactionAsyncHeap<TIndex, TItem> {
+    readonly getChanges: TGetChanges<TChangeIndex, TIndex, TItem>
+}
+
+// endregion
+
